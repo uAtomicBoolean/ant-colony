e4956d5f6bea51c832cd62160a68ff8897d64f99
Benjamin Guirlet <benjamin.guirlet@ecole.ensicaen.fr>
2023-12-01 09:46:02 +0100

    fix: Namespace not found. The namespace 'simulateur::colonie' wasn't found by either CLion and g++. The only solution was to rename the folder, file and namespace of the Colonie class and then rename it to 'colonie'.

--- app/include/simulateur/simulateur.h
+++ app/include/simulateur/simulateur.h
@@ -1,4 +1,4 @@
-#ifndef ANT_COLONY_COLONIE_H
+#ifndef ANT_COLONY_TAMERE_H
 #define ANT_COLONY_COLONIE_H
 
 #include "simulateur/carte/carte.h"
@@ -8,6 +8,7 @@
 namespace simulateur {
     class Simulateur {
     private:
+        simulateur::colonie::Colonie colonie;
         simulateur::carte::Carte carte;
     public:
         void genere_carte();
