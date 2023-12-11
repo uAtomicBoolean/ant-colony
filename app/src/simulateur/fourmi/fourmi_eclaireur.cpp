0c10a82623251346e2535c09b31346d4ae39ffc7
Benjamin Guirlet <benjamin.guirlet@ecole.ensicaen.fr>
2023-12-11 15:09:47 +0100

    chore: Removes couts.

--- app/src/simulateur/fourmi/fourmi_eclaireur.cpp
+++ app/src/simulateur/fourmi/fourmi_eclaireur.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "fourmi_eclaireur.h"
 #include "carte.h"
 #include "simulateur.h"
@@ -10,12 +9,9 @@ namespace sim::fourmi {
         bool a_fini = false;
         while (!a_fini) {
             //  Explore au hasard, bloquer par les obstacles
-            std::cout << this->get_case_actuelle()->get_position().x << " "
-                      << this->get_case_actuelle()->get_position().y << std::endl;
             sim::carte::Case *case_actuelle{this->get_case_actuelle()};
             sim::carte::Case *case_voisine = sim::carte::Carte::get_case_voisine8d(case_actuelle);
-            std::cout << "case voisine : " << case_voisine->get_position().x << " " << case_voisine->get_position().y
-                      << std::endl;
+
             // Regarde si la case voisine est valide
             bool est_valide = sim::carte::Carte::check_case(case_voisine);
             if (est_valide) {
@@ -25,7 +21,6 @@ namespace sim::fourmi {
                 this->explorer();
                 case_actuelle->set_nb_fourmis(-1);
                 case_voisine->set_nb_fourmis(1);
-                std::cout << "case valide" << std::endl;
             }
         }
     }
